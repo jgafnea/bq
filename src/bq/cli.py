4cc07abf9a732e6789361332cba89b25f14be77e
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-06-03 16:07:45 -0500

    fix: use relative imports

--- src/bq/cli.py
+++ src/bq/cli.py
@@ -1,37 +1,31 @@
 import argparse
 import sys
 
-from rich.console import Console
-
-from bq import display, search
-
-console = Console()
+from .display import console, display
+from .search import search
 
 
 def main():
-    print("Hello World")
-    # parser = argparse.ArgumentParser(
-    #     description="Search LibGen and return download links"
-    # )
-    # parser.add_argument("title", type=str, help="Title of book to search for")
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Search LibGen and return download links"
+    )
+    parser.add_argument("title", type=str, help="Title of book to search for")
+    args = parser.parse_args()
 
-    # try:
-    #     results = search(args.title)
-    #     if not results:
-    #         console.print(
-    #             f"[bold yellow]No results found[/] for: [italic]'{args.title}'[/]"
-    #         )
-    #         sys.exit(1)
-    #     display(results)
+    try:
+        results = search(args.title)
+        if not results:
+            console.print(f"[bold yellow]No results: [/] [bold]'{args.title}'[/]")
+            sys.exit(1)
+        display(results)
 
-    # except KeyboardInterrupt:
-    #     console.print("[red]Search canceled by user.[/]")
-    #     sys.exit(130)
+    except KeyboardInterrupt:
+        console.print("[red]Search canceled by user.[/]")
+        sys.exit(130)
 
-    # except Exception as e:
-    #     console.print(f"[bold red]An error occurred:[/] {e}")
-    #     sys.exit(1)
+    except Exception as e:
+        console.print(f"[bold red]An error occurred:[/] {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
